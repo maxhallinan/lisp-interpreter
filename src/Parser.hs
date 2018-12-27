823216b5476e9aad5d2c4d863f6ee260e1a098f3
Max Hallinan <maxhallinan@riseup.net>
2018-12-27 15:02:32 +0100

    Parse a small subset of Scheme syntax

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,89 +1,101 @@
 module Parser where
 
-import Control.Applicative ((<|>))
-import Control.Monad (liftM)
-import qualified Text.Megaparsec as Mega
+import Control.Applicative ((<|>), empty)
+import Control.Monad.Combinators (between)
 import qualified Text.Megaparsec.Char as Char
+import qualified Text.Megaparsec.Char.Lexer as Lex
+import qualified Text.Megaparsec.Expr as Expr
+import qualified Text.Megaparsec as Mega
+import qualified Text.Megaparsec.Error as Err
 import Data.Void (Void)
 
 type Parser = Mega.Parsec Void String
 
-data LispValue
-  = LispImproperList [LispValue] LispValue
-  | LispProperList [LispValue]
-  | LispAtom String
-  | LispNumber Integer
-  | LispString String
-  | LispBool Bool
+type ParseError = Err.ParseError Char Void
+
+data LispVal
+  = Atom String
+  | Number Integer
+  | String String
+  | Bool Bool
+  | ProperList [LispVal]
+  | ImproperList [LispVal] LispVal
   deriving (Eq, Show)
 
--- Datum
--- <datum> := <atom> | <boolean> | <number> | <string> | <list>
+parse :: String -> String -> Either ParseError LispVal
+parse filename input = Mega.parse lispSyntax filename input
 
-lispDatum :: Parser LispValue
-lispDatum = lispList <|> lispAtom <|> lispBool <|> lispNumber
+lispSyntax :: Parser LispVal
+lispSyntax = between space Mega.eof lispVal
 
-lispAtom :: Parser LispValue
-lispAtom = do
-  first <- Char.letterChar <|> Char.symbolChar <|> Char.asciiChar
-  rest <- Mega.many (Char.letterChar <|> Char.digitChar <|> Char.symbolChar)
+lispVal :: Parser LispVal
+lispVal = lexeme $
+      bool 
+  <|> atom 
+  <|> number 
+  <|> string 
+  <|> list
+
+space :: Parser ()
+space = Lex.space Char.space1 lineComment blockComment
+  where
+    lineComment :: Parser ()
+    lineComment = Lex.skipLineComment ";"
+
+    blockComment :: Parser ()
+    blockComment = Lex.skipBlockComment ";" "\n"
+
+lexeme :: Parser a -> Parser a
+lexeme = Lex.lexeme space
+
+symbol :: String -> Parser String
+symbol = Lex.symbol space
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+atom :: Parser LispVal
+atom = do
+  first <- Char.letterChar <|> Char.symbolChar
+  rest <- Mega.many Char.letterChar
   let atom = first : rest
-  return $ case atom of
-              "#t" -> LispBool True
-              "#f" -> LispBool False
-              _    -> LispAtom atom
+  return $ Atom atom
 
-lispNumber :: Parser LispValue
-lispNumber = do
-  number <- Mega.many Char.digitChar
-  return $ LispNumber (read number)
-
-lispString :: Parser LispValue
-lispString = do
-  Char.char '"'
-  stringValue <- Mega.many (Char.noneOf "\"")
-  Char.char '"'
-  return $ LispString stringValue
-
-lispBool :: Parser LispValue
-lispBool = do
+bool :: Parser LispVal
+bool = do
   Char.char '#'
   bool <- toBool <$> (Char.char 't' <|> Char.char 'f')
-  return $ LispBool bool
+  return $ Bool bool
   where
     toBool 'f' = False
     toBool 't' = True
 
-lispList :: Parser LispValue
-lispList = do 
-  Char.char '('
-  Char.space
-  list <- Mega.try lispImproperList <|> lispProperList
-  Char.char ')'
-  return list
+number :: Parser LispVal
+number = do
+  number <- Mega.some Char.digitChar
+  return $ Number (read number)
 
-lispProperList :: Parser LispValue
-lispProperList = LispProperList <$> sepBySpace lispDatum
+string :: Parser LispVal
+string = do
+  Char.char '"'
+  stringValue <- Mega.many (Char.noneOf "\"")
+  Char.char '"'
+  return $ String stringValue
 
-lispImproperList :: Parser LispValue
-lispImproperList = do
-  head <- Mega.endBy lispDatum (Mega.some Char.spaceChar)
-  tail <- Char.char '.' >> Char.space >> lispDatum
-  return $ LispImproperList head tail
-
--- Helpers
-
-sepBySpace :: Parser a -> Parser [a]
-sepBySpace = (flip Mega.endBy) (Mega.some Char.spaceChar)
-
-beginBySpace :: Parser a -> Parser a
-beginBySpace parser = do
-  Char.space
-  x <- parser
+list :: Parser LispVal
+list = do 
+  symbol "("
+  x <- Mega.try improperList <|> properList
+  symbol ")"
   return x
 
-endBySpace :: Parser a -> Parser a
-endBySpace parser = do
-  x <- parser
-  Char.space
-  return x
+properList :: Parser LispVal
+properList = do
+  terms <- Mega.many lispVal
+  return $ ProperList terms
+
+improperList :: Parser LispVal
+improperList = do
+  head <- Mega.manyTill lispVal (symbol ".")
+  tail <- lispVal
+  return $ ImproperList head tail
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,54 +1,55 @@
 module ParserSpec (main) where
 
-import Text.Megaparsec (parseMaybe)
 import Test.Hspec (describe, hspec, it, shouldBe)
 import Test.QuickCheck
-import Parser
+import qualified Parser
+
+parse = Parser.parse "fake-filename"
 
 main :: IO ()
 main = hspec $ do
-  describe "Parser" $ do
+  describe "parse" $ do
+    describe "comment" $ do
+      it "ignores a comment" $ do
+        parse "#t;foo" `shouldBe` Right (Parser.Bool True)
+        
     describe "boolean" $ do
       it "parses a boolean" $ do
-        parseMaybe Parser.lispBool "#t" `shouldBe` Just (LispBool True)
-        parseMaybe Parser.lispBool "#f" `shouldBe` Just (LispBool False)
+        parse "#t" `shouldBe` Right (Parser.Bool True)
+        parse "#f" `shouldBe` Right (Parser.Bool False)
 
     describe "number" $ do
       it "parses a number" $ do
-        parseMaybe Parser.lispNumber "1" `shouldBe` Just (LispNumber 1)
-        parseMaybe Parser.lispNumber "123" `shouldBe` Just (LispNumber 123)
+        parse "1" `shouldBe` Right (Parser.Number 1)
+        parse "123" `shouldBe` Right (Parser.Number 123)
 
     describe "string" $ do
       it "parses a string" $ do
-        parseMaybe Parser.lispString "\"a\"" `shouldBe` Just (LispString "a")
-        parseMaybe Parser.lispString "\"abc\"" `shouldBe` Just (LispString "abc")
-        parseMaybe Parser.lispString "\"1abc\"" `shouldBe` Just (LispString "1abc")
-        parseMaybe Parser.lispString "\"ab1c\"" `shouldBe` Just (LispString "ab1c")
+        parse "\"a\"" `shouldBe` Right (Parser.String "a")
+        parse "\"abc\"" `shouldBe` Right (Parser.String "abc")
+        parse "\"1abc\"" `shouldBe` Right (Parser.String "1abc")
+        parse "\"ab1c\"" `shouldBe` Right (Parser.String "ab1c")
 
     describe "atom" $ do
-      it "parses a boolean" $ do
-        parseMaybe Parser.lispAtom "#t" `shouldBe` Just (LispBool True)
-        parseMaybe Parser.lispAtom "#f" `shouldBe` Just (LispBool False)
-
       it "parses an atom" $ do
-        parseMaybe Parser.lispAtom "a" `shouldBe` Just (LispAtom "a")
-        parseMaybe Parser.lispAtom "abc" `shouldBe` Just (LispAtom "abc")
+        parse "a" `shouldBe` Right (Parser.Atom "a")
+        parse "abc" `shouldBe` Right (Parser.Atom "abc")
 
     describe "list" $ do
       describe "proper list" $ do
         it "parses with spaces" $ do
-          parseMaybe Parser.lispList "( a 1 )" `shouldBe` Just (LispProperList [LispAtom "a", LispNumber 1])
+          parse "( a 1 )" `shouldBe` Right (Parser.ProperList [Parser.Atom "a", Parser.Number 1])
 
         it "parses without spaces" $ do
-          parseMaybe Parser.lispList "(a 1)" `shouldBe` Just (LispProperList [LispAtom "a", LispNumber 1])
+          parse "(a 1)" `shouldBe` Right (Parser.ProperList [Parser.Atom "a", Parser.Number 1])
 
       describe "improper list" $ do
         it "parses with spaces" $ do
-          parseMaybe Parser.lispList "( a . 1 )" `shouldBe` Just (LispImproperList [LispAtom "a"] (LispNumber 1))
+          parse "( a . 1 )" `shouldBe` Right (Parser.ImproperList [Parser.Atom "a"] (Parser.Number 1))
 
         it "parses without spaces" $ do
-          parseMaybe Parser.lispList "(a . 1)" `shouldBe` Just (LispImproperList [LispAtom "a"] (LispNumber 1))
+          parse "(a . 1)" `shouldBe` Right (Parser.ImproperList [Parser.Atom "a"] (Parser.Number 1))
 
       describe "nested list" $ do
-          it "parses a nested list" $ do
-            parseMaybe Parser.lispList "(\"abc\" (a . 1))" `shouldBe` Just (LispProperList [LispString "abc", LispImproperList [LispAtom "a"] (LispNumber 1)])
+        it "parses a nested list" $ do
+          parse "(\"abc\" (a . 1))" `shouldBe` Right (Parser.ProperList [Parser.String "abc", Parser.ImproperList [Parser.Atom "a"] (Parser.Number 1)])
